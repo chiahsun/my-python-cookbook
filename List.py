e163526b11eea8efea9c4cdcefeae04fcf2968d8
chiahsun <chiahsun0814@gmail.com>
2022-10-06 15:09:24 +0800

    Add list comprehension

---
+++ List.py
@@ -0,0 +1,23 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # List
+# ## List Comprehension
+
+# %%
+print([a ** 2 for a in range(10) if a % 3 == 0])
+
+# %%
