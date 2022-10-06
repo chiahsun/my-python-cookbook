05805b32d82a58abb3aa2b5050107f135c05f118
chiahsun <chiahsun0814@gmail.com>
2022-10-06 14:43:56 +0800

    Add zip

---
+++ Zip.py
@@ -0,0 +1,30 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+a = [1, 2, 3]
+b = ["One", "Two", "Three"]
+print(list(zip(a, b)))
+
+# %%
+print(*zip(a, b))
+
+# %%
+c, d = zip(*zip(a, b))
+print(c)
+print(d)
+print(list(zip(*zip(a, b))))
+
+# %%
