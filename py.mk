e11633900f811a7dac8e5c1f3ef9d90634d6a4de
chiahsun <chiahsun0814@gmail.com>
2024-01-04 01:12:37 +0800

    Add usage for pyenv

--- py.mk
+++ py.mk
@@ -1,5 +1,6 @@
 
 # https://stackoverflow.com/questions/2483182/recursive-wildcards-in-gnu-make
+PYC=python
 PY_FILES=$(shell find . -type f -name '*.py')
 LOG_FILES=$(patsubst %.py, %.py.log, $(PY_FILES))
 
@@ -10,7 +11,7 @@ echo:
 compile: $(LOG_FILES)
 
 %.py.log: %.py
-	python3 $< > $@
+	$(PYC) $< > $@
 
 clean:
 	rm -f $(LOG_FILES)
